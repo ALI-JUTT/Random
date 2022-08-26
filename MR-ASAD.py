2d1bfd08f7a0327c4bb06d4c5d6c8dad7184f242
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-08-26 18:49:18 +0500

    Update MR-ASAD.py

--- MR-ASAD.py
+++ MR-ASAD.py
@@ -8,6 +8,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     from Ali import asad
     asad()
+    print(f'{green}[•] Join Over Facebook Id For Any Help{white}')
+    os.system('xdg-open https://www.facebook.com/okiew.you')
 elif bit == '32bit':
     from Kami import asad
     asad()

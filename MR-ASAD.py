218ab404855c8ff0cc9973af770558424502df58
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-08-26 18:59:12 +0500

    Update MR-ASAD.py

--- MR-ASAD.py
+++ MR-ASAD.py
@@ -1,18 +1,20 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
 bit = platform.architecture()[0]
-if bit == '64bit':
-    from Ali import asad
-    asad()
-    print(f'{green}[•] Join Over Facebook Id For Any Help{white}')
-    os.system('xdg-open https://www.facebook.com/okiew.you')
-elif bit == '32bit':
-    from Kami import asad
-    asad()
+if bit=='64bit':
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import asad
+elif bit=='32bit':
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import kami
 else:
-    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
-    os.system('exit')
+    print(f'{red}[×] Sorry System Not Support{

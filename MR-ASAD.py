168364d9834784e14a8cbf72b3aee91041aa59d1
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2022-08-26 19:04:34 +0500

    Update MR-ASAD.py

--- MR-ASAD.py
+++ MR-ASAD.py
@@ -1,20 +1,16 @@
-import os,time,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-green = ('\033[1;32m')
-white = ('\033[1;37m')
-red = ('\033[1;31m')
-
-print('<------------------------------------>')
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
 bit = platform.architecture()[0]
-if bit=='Asad':
-    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
-    import asad
-elif bit=='Kami':
-    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
-    import kami
+if bit == '64bit':
+    from Ali import asad
+    asad()
+elif bit == '32bit':
+    from Kami import asad
+    asad()
 else:
-    print(f'{red}[×] Sorry System Not Support{
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
